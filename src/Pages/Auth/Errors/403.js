59a13545bcf4755770f9b236866f3cc6609990e2
Farid Mohamed <fm221210@gmail.com>
2025-07-26 01:24:38 +0300

    Modify in the go of users after login or register depends on them role

--- src/Pages/Auth/Errors/403.js
+++ src/Pages/Auth/Errors/403.js
@@ -8,7 +8,7 @@ export default function Error403({role}) {
       </div>
       <div className='subtitle'>
         Oops, You don't have permission to access this page.
-        <Link className='d-block text-center btn btn-primary mt-5' to={role === '1996' ? "/dashboard/writer" : "/"}>
+        <Link className='d-block text-center btn btn-primary mt-5' to={role === '2001' &&"/"}>
           {role === "1996" ? "Go To Writer Page" : "Return To Home Page"}
         </Link>
       </div>
